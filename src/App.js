46b26bcb522abf3d6bf49a98951720f1355d331b
Oleh Daybov <olegtranslator@gmail.com>
2016-02-14 22:15:43 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import { DragDropContext } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+import Source from './Source';
+import Destination from './Destination';
 
 export default class App extends Component {
   render() {
     return (
-      <h1>Hello, world.</h1>
+      <div>
+        <div style={{ overflow: 'hidden', clear: 'both' }}>
+          <Destination />
+        </div>
+        <div style={{ overflow: 'hidden', clear: 'both' }}>
+          <Source name="OurBox" />
+        </div>
+      </div>
     );
   }
 }
+
+export default DragDropContext(HTML5Backend)(App);
